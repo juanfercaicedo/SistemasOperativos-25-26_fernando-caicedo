9ea1e81515b1069fe87c583138981a708cdb9961
Juan Fernando Caicedo <jfcaicedo449@gmail.com>
2026-02-18 17:45:39 +0100

    ahora con interfaz de usuario

--- PrimerasClases/002printf.c
+++ PrimerasClases/002printf.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-int suma(int x, int y); // Esto es una declaración (prototipo).
+int suma(int , int ); // Esto es una declaración (prototipo).
 						// Es necesario porque main() usa suma() antes de que esté definida.
 
 int main(void)
 {
-	int x = 5, y = 6;
-	
+	int x , y;
+	puts("Introduce el valor de la primera variable:");
+	scanf("%d", &x);
+	puts("Introduce el valor de la segunda variable:");
+	scanf("%d", &y);
+
 	printf("x vale %i, y vale %i y su suma %i \n", x, y, suma(x,y));
 	
 	return 0;
