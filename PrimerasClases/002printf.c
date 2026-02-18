f35d65c316e04c5a5bb3d203416b7e424b054c77
Juan Fernando Caicedo <jfcaicedo449@gmail.com>
2026-02-18 17:39:14 +0100

    código hecho en clase printf

--- PrimerasClases/002printf.c
+++ PrimerasClases/002printf.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
+int suma(int x, int y); // Esto es una declaración (prototipo).
+						// Es necesario porque main() usa suma() antes de que esté definida.
+
 int main(void)
 {
+	int x = 5, y = 6;
 	
+	printf("x vale %i, y vale %i y su suma %i \n", x, y, suma(x,y));
 	
 	return 0;
 }
 
+int suma(int x, int y)
+{
+	return x+y;
+}
