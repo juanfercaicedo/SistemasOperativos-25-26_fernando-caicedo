9155437ea8ede95de1169990f7262abd25a33732
juanfercaicedo <juan.caicedo1@alumnos.uneatlantico.es>
2026-02-26 16:03:05 +0100

    cadenas

---
+++ 26Feb/cadenas.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h> // siempre que ocupemos cadenas debemos incluir esta cabecera
+
+int main(void){
+    char nombre[15];
+    printf("Introduzca su primer nombre: ");
+    scanf("%s", nombre);
+}
