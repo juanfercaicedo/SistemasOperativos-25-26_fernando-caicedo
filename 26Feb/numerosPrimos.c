ce420c844909175a7c67e061a8b5c40f5204efa2
juanfercaicedo <juan.caicedo1@alumnos.uneatlantico.es>
2026-02-26 15:36:09 +0100

    números primos

---
+++ 26Feb/numerosPrimos.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+    int x, y;
+
+    printf("Introduce el primer número:")
+    scanf("%d", &x);
+
+    printf("Ahora introduce el segundo número: ")
+    scanf("%d", &y);
+
+    
+
+}
