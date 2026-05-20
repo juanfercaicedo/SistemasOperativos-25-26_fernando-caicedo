b85be2f0c57c32f84935c2947404c5ab33a626de
juanfercaicedo <juan.caicedo1@alumnos.uneatlantico.es>
2026-05-20 16:58:47 +0200

    código inicial enunciado exámen final año 24-25

---
+++ PracticaExamenFinal/examenFinal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <signal.h>
+
+#define ROWS = 4
+#define COLS = 3
+
+int matrix[ROWS][COLS], ipos, jpos, temp;
+pthread_t positivo, negativo, coordinador;
+
+void muestra_tabular();
+void positivo_handler(int);
+void negativo_handler(int);
+void coordinador_handler(int);
+void* positivo_f();
+void* negativo_f();
+void* coordinador_thread_f();
+
+int main(void) {
+
+}
